e6de332fe86eb41fce94adb9ec96c575f1fb996b
Tabish <mtabishkhanday@gmail.com>
2021-02-02 12:00:42 +0530

    day 2 flask dev

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date_posted': 'Febuary 01, 2020'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second Post content',
+        'date_posted': 'Febuary 02, 2020'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def homepage():
-    return "<h1>Homepage !</h1>"
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page !</h1>"
+    return render_template("about.html", title='About')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
